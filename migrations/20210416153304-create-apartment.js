e64b2c05cde53a1ba3804a0963a5b6d92f12df63
dev <dev@rabbit.local>
2021-04-16 09:44:52 -0600

    add apartment model

---
+++ migrations/20210416153304-create-apartment.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Apartments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      floorAreaSize: {
+        type: Sequelize.FLOAT,
+      },
+      numberOfRooms: {
+        type: Sequelize.INTEGER,
+      },
+      latitude: {
+        type: Sequelize.FLOAT,
+      },
+      longitude: {
+        type: Sequelize.FLOAT,
+      },
+      realtorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'users',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Apartments');
+  },
+};
---
+++ models/apartment.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Apartment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Apartment.belongsTo(models.User, {
+        foreignKey: 'realtorId',
+        as: 'realtor',
+        onDelete: 'cascade',
+      });
+    }
+  }
+  Apartment.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: 3,
+            msg: 'Name must be at least 3 characters in length',
+          },
+        },
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: {
+            args: 10,
+            msg: 'Description must be at least 10 characters in length',
+          },
+        },
+      },
+      floorAreaSize: DataTypes.FLOAT,
+      numberOfRooms: DataTypes.INTEGER,
+      latitude: DataTypes.FLOAT,
+      longitude: DataTypes.FLOAT,
+      realtorId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Apartment',
+    },
+  );
+  return Apartment;
+};
